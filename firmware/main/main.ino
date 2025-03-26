66d8db7de1a9aa591f3947b41926d6aaf1767db5
Jittania <jittania@gmail.com>
2025-03-25 22:35:59 -0700

    Cleaned up main.ino file

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -1,3 +1,7 @@
+// ==================================================================
+// main.ino
+// Entry point: initializes all modules and runs main event loop
+// ==================================================================
 #include <ADC.h>
 #include <DMAChannel.h>
 #include <ILI9341_t3n.h>
@@ -17,11 +21,6 @@
 #include "keyboard/keyboard.h"
 #include "keyboard/keyboard.cpp"
 
-/**
- * The main/top-level setup function that initializes the serial communication, SPI, and I2C;
- * calls other setup functions to configure the screen, receiver, transmitter, and keyboard poller.
- * Usage: The first function called in the program to initialize all components.
- */
 void setup() {
   // Initializes serial communication with Teensy at baud rate of 9600 bps:
   Serial.begin(9600);
@@ -37,7 +36,10 @@ void setup() {
   // Specifies 12-bit resolution:
   analogReadResolution(12);
 
+  // Testing purposes only:
   test_incoming_message.begin(incoming_message_callback, 1000000);
+
+  // Module setup:
   setup_screen();
   setup_receiver();
   setup_transmitter();
@@ -46,11 +48,8 @@ void setup() {
   Serial.println("setup() complete\n============================");
 }
 
-/**
- * Continuously polls the battery voltage and updates the display with batt charge level.
- */
 void loop() {
-  // uint16_t val = 2048 + 2047 * sin(2*3.14159*micros()/1e6 * 1.5e3); // tryna make a number between 0 and 2^12 i.e. 12 bits
+  // uint16_t val = 2048 + 2047 * sin(2*3.14159*micros()/1e6 * 1.5e3);
 
   poll_battery();
 }
